4036ff910df60cb51c79cce75da9807e25606326
zhangyuhao018 <zhangyuhao018@gmail.com>
2021-03-26 12:38:59 +0900

    实现ConnectionActivity类,实施ConnectionActivity布局,配置资源XML

---
+++ app/src/main/java/com/dji/FPVDemo/ConnectionActivity.java
@@ -0,0 +1,15 @@
+package com.dji.FPVDemo;
+
+import android.app.Activity;
+import android.view.View;
+
+public class ConnectionActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = ConnectionActivity.class.getName();
+
+    private TextView mTextConnectionStatus;
+    private TextView mTextProduct;
+    private TextView mVersionTv;
+    private Button mBtnOpen;
+
+}
