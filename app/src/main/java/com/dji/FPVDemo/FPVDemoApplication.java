94f57282e9791989b8cfbdb61d8ecb857e2729c5
zhangyuhao018 <zhangyuhao018@gmail.com>
2021-03-26 12:20:47 +0900

    创建MApplication，创建FPVDemoApplication,实现MainActivity类,实施MainActivity布局

---
+++ app/src/main/java/com/dji/FPVDemo/FPVDemoApplication.java
@@ -0,0 +1,10 @@
+package com.dji.FPVDemo;
+
+import android.app.Application;
+
+public class FPVDemoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/dji/FPVDemo/MainActivity.java
+++ app/src/main/java/com/dji/FPVDemo/MainActivity.java
@@ -2,13 +2,119 @@ package com.dji.FPVDemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener, View.OnClickListener {
+
+    protected TextureView mVideoSurface = null;
+    private Button mCaptureBtn,mShootPhotoModeBtn, mRecordVideoModeBtn;
+    private ToggleButton mRecordBtn;
+    private TextView recordingTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    public void onReturn(View view) {
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+    private void initUI() {
+        //init mVideoSurface
+        mVideoSurface = (TextureView) findViewById(R.id.video_previewer_surface);
+
+        recordingTime = (TextView) findViewById(R.id.timer);
+        mCaptureBtn = (Button) findViewById(R.id.btn_capture);
+        mRecordBtn = (ToggleButton) findViewById(R.id.btn_record);
+        mShootPhotoModeBtn = (Button) findViewById(R.id.btn_shoot_photo_mode);
+        mRecordVideoModeBtn = (Button) findViewById(R.id.btn_record_video_mode);
+
+        if (null != mVideoSurface) {
+            mVideoSurface.setSurfaceTextureListener(this);
+        }
+
+        mCaptureBtn.setOnClickListener(this);
+        mRecordVideoModeBtn.setOnClickListener(this);
+        mShootPhotoModeBtn.setOnClickListener(this);
+        mRecordVideoModeBtn.setOnClickListener(this);
+
+        recordingTime.setVisibility(View.INVISIBLE);
+
+        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.btn_capture:{
+                break;
+            }
+            case R.id.btn_shoot_photo_mode:{
+                break;
+            }
+            case R.id.btn_record_video_mode:{
+                break;
+            }
+            default:
+                break;
+        }
     }
 }
